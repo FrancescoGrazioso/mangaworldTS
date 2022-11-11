d6c1e4444b0f0c575031e68950f521b3b48622f5
Francesco Grazioso <40018163+FrancescoGrazioso@users.noreply.github.com>
2022-11-11 16:57:20 +0100

    edited test file for a better testing suite

--- src/__tests__/test.ts
+++ src/__tests__/test.ts
@@ -1,13 +1,28 @@
 import { Manga } from './../model/Manga';
 import { getMangaFromUrl, searchMangaByKeywords } from '../index';
 
+const searchTerm = 'one piec'
+
+test ('Given keyword checks that e result is given',async () => {
+  const mangaSearch = await searchMangaByKeywords(searchTerm);
+  expect(mangaSearch.length).toBeGreaterThan(0);
+})
+
 test('Given a URL checks that a manga is responded', async () => {
-  const manga: Manga = await getMangaFromUrl("https://www.mangaworld.so/manga/1637/chainsaw-man")
+  const mangaSearch = await searchMangaByKeywords(searchTerm);
+  const manga: Manga = mangaSearch[0];
   expect(manga.title).not.toBe(undefined);
 });
 
-test ('Given keyword checks that e result is given',async () => {
-  const mangaSearch = await searchMangaByKeywords('chainsaw');
-  console.log(mangaSearch)
-  expect(mangaSearch.length).toBeGreaterThan(0);
+test ('After reading a manga object checks that it ha chapters',async () => {
+  const mangaSearch = await searchMangaByKeywords(searchTerm);
+  const manga: Manga = mangaSearch[0];
+  expect(manga.chapters.length).toBeGreaterThan(0);
+})
+
+test ('Checks that manga chapter is initialized correctly',async () => {
+  const mangaSearch = await searchMangaByKeywords(searchTerm);
+  const manga: Manga = mangaSearch[0];
+  const chapter = manga.chapters[0];
+  expect(chapter.url).not.toBe(undefined);
 })
