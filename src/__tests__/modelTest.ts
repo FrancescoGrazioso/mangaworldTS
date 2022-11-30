5b5846ea3c69789755e9595bf598862195f13ead
Francesco Grazioso <40018163+FrancescoGrazioso@users.noreply.github.com>
2022-11-30 20:53:41 +0100

    fixed bugs

--- src/__tests__/modelTest.ts
+++ src/__tests__/modelTest.ts
@@ -2,47 +2,55 @@ import { getMangaFromUrl } from '../index';
 import { Manga } from '../model/Manga';
 import { getChapterDetail, searchMangaByKeywords, searchMangaURLsByKeywords } from '../index';
 
-const searchTerm = 'one pi'
+const searchTerm = 'one pi';
 
 describe('Manga query utilities', () => {
-  test ('Given keyword checks that e result is given',async () => {
+  test('Given keyword checks that e result is given', async () => {
     const mangaSearch = await searchMangaByKeywords(searchTerm);
     expect(mangaSearch.length).toBeGreaterThan(0);
-  })
-})
+  });
+});
 
-describe('Manga info utilities', ()=>{
+describe('Manga info utilities', () => {
   test('Given a URL checks that a manga is responded', async () => {
     const manga: Manga = await getMangaFromUrl('https://www.mangaworld.so/manga/1848/blue-lock/');
     expect(manga.title).not.toBe(undefined);
   });
-  
-  test ('After reading a manga object checks that it has chapters',async () => {
+
+  test('After reading a manga object checks that it has chapters', async () => {
     const manga: Manga = await getMangaFromUrl('https://www.mangaworld.so/manga/1848/blue-lock/');
-    expect(manga.chapters.length).toBeGreaterThan(0);
-  })
-})
+    expect(manga.chapters).not.toBeUndefined();
+    if (manga.chapters) expect(manga.chapters.length).toBeGreaterThan(0);
+  });
+});
 
-describe('Chapter info utilities', ()=>{
-  test ('Checks that manga chapter is initialized correctly',async () => {
+describe('Chapter info utilities', () => {
+  test('Checks that manga chapter is initialized correctly', async () => {
     const mangaSearch = await searchMangaURLsByKeywords(searchTerm);
     const manga: Manga = await getMangaFromUrl(mangaSearch[0]);
-    let chapter = manga.chapters[0];
-    expect(chapter.url).not.toBe(undefined);
-  })
-  
-  test ('Checks chapter detailed are retrieved correctly',async () => {
+    expect(manga.chapters).not.toBeUndefined();
+    if (manga.chapters) {
+      let chapter = manga.chapters[0];
+      expect(chapter.url).not.toBe(undefined);
+    }
+  });
+
+  test('Checks chapter detailed are retrieved correctly', async () => {
     const mangaSearch = await searchMangaURLsByKeywords(searchTerm);
     const manga: Manga = await getMangaFromUrl(mangaSearch[0]);
-    let chapter = manga.chapters[0];
-    chapter = await getChapterDetail(chapter);
 
-    expect(chapter.pageUrl).not.toBe(undefined);
-    if(chapter.pageUrl) expect(chapter.pageUrl[0]).not.toBe(undefined);
+    expect(manga.chapters).not.toBeUndefined();
+    if (manga.chapters) {
+      let chapter = manga.chapters[0];
+      chapter = await getChapterDetail(chapter);
 
-    expect(chapter.keywords?.length).toBeGreaterThan(0);
-    expect(chapter.visual).toBeGreaterThan(0);
-    expect(chapter.visualToday).toBeGreaterThan(0);
-    expect(chapter.volume).toBeGreaterThan(0);
-  })
-})
+      expect(chapter.pageUrl).not.toBe(undefined);
+      if (chapter.pageUrl) expect(chapter.pageUrl[0]).not.toBe(undefined);
+
+      expect(chapter.keywords?.length).toBeGreaterThan(0);
+      expect(chapter.visual).toBeGreaterThan(0);
+      expect(chapter.visualToday).toBeGreaterThan(0);
+      expect(chapter.volume).toBeGreaterThan(0);
+    }
+  });
+});
--- src/services/scrapers/BaseScraper.ts
+++ src/services/scrapers/BaseScraper.ts
@@ -1,16 +1,13 @@
-
+import fetch from 'node-fetch';
+import { JSDOM } from 'jsdom';
 
 export async function fetchHTMLWebPage(url: string) {
-  //const urlToFetch = 'http://www.whateverorigin.org/get?url=' + encodeURIComponent(url) + '&callback=?'
   const res = await fetch(url);
-  let html: string = await res.text();
-  // html = html.slice(2, html.length-1).trim();
-  // const jsonHTML = JSON.parse(html)
-  // return jsonHTML.contents;
+  const html: string = await res.text();
   return html;
 }
 
 export async function parseHTML(html: string) {
-  const dom:Document = new DOMParser().parseFromString(html, "text/html");
+  const dom: Document = (new JSDOM(html)).window.document
   return dom;
 }
